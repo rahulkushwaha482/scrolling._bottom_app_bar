a909ffd20f62cd1ac48e1ee87c2c016723fe84c4
rahulkushwaha482 <rahulkushwaha482@gmail.com>
2022-09-24 18:14:36 +0530

    scrolling bottom app bar is added to the github repository

---
+++ android/app/src/main/kotlin/com/example/scroll_bootom_bar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.scroll_bootom_bar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/my_home_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyHomePage> {
+  late final ScrollListener _model;
+  late final ScrollController _controller;
+  final double _bottomNavBarHeight = 90;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = ScrollController();
+    _model = ScrollListener.initialise(_controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: AnimatedBuilder(
+        animation: _model,
+        builder: (context, child) {
+          return Stack(
+            children: [
+              ListView.builder(
+                controller: _controller,
+                itemCount: 50,
+                itemBuilder: (_, i) => ListTile(title: Text('Item $i')),
+              ),
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: _model.bottom,
+                child: _bottomNavBar,
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  Widget get _bottomNavBar {
+    return SizedBox(
+      height: _bottomNavBarHeight,
+      child: BottomNavigationBar(
+        backgroundColor: Colors.orangeAccent,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.call), label: 'Call'),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Message'),
+        ],
+      ),
+    );
+  }
+}
+
+class ScrollListener extends ChangeNotifier {
+  double bottom = 0;
+  double _last = 0;
+
+  ScrollListener.initialise(ScrollController controller, [double height = 90]) {
+    controller.addListener(() {
+      final current = controller.offset;
+      bottom += _last - current;
+      if (bottom <= -height) bottom = -height;
+      if (bottom >= 0) bottom = 0;
+      _last = current;
+      if (bottom <= 0 && bottom >= -height) notifyListeners();
+    });
+  }
+}
